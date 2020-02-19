c6dcf1d6c33155859f9f4ce9d65cf50df523733b
Dusty Garon <hello@dustygaron.com>
2020-02-19 15:05:14 -0500

    Fixed redux store

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,28 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducer from './reducers'
+import reducer from './reducers'
 
 const initialState = {}
 const middleware = [thunk]
 
 // createStore() creates a Redux store that holds the complete state tree of your app. There should only be a single store in your app.
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+// const store = createStore(rootReducer, initialState, compose(
+//   applyMiddleware(...middleware),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+// )
+
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+);
+const store = createStore(reducer, enhancer);
 
 export default store
